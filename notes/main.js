5f111ea37c6a6407459dfe9054f8765feaea5238
chiayiju <57925324+chiayiju@users.noreply.github.com>
2024-12-17 20:58:01 +0800

    Add files via upload

---
+++ notes/main.js
@@ -0,0 +1,18 @@
+(function() {
+	const theme = localStorage.getItem('theme') || 'dark';
+	document.documentElement.setAttribute('data-theme', theme);
+	
+	document.addEventListener("DOMContentLoaded", function() {
+		const logo = document.getElementById("logo");
+		if (logo) {
+			logo.src = theme === "dark" ? "../images/logo-dark.webp" : "../images/logo-light.webp";
+		}
+		const toggleButton = document.getElementById("theme-toggle");
+		if (toggleButton) {
+			toggleButton.innerHTML = theme === "dark" ? '&#9790;' : '&#9728;';
+			toggleButton.style.backgroundColor = theme === "dark" ? '#333' : '#f5f5f5';
+			toggleButton.style.color = theme === "dark" ? '#f5f5f5' : '#333';
+		}
+		document.body.style.display = "block"; // Show body after setting the theme
+	});
+})();
---
+++ notes/toggletheme.js
@@ -0,0 +1,17 @@
+function toggleTheme() {
+	const currentTheme = document.documentElement.getAttribute('data-theme');
+	const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+	document.documentElement.setAttribute('data-theme', newTheme);
+	localStorage.setItem('theme', newTheme);
+
+	const logo = document.getElementById("logo");
+	if (logo) {
+		logo.src = newTheme === "dark" ? "../images/logo-dark.webp" : "../images/logo-light.webp";
+	}
+	const toggleButton = document.getElementById("theme-toggle");
+		if (toggleButton) {
+			toggleButton.innerHTML = newTheme === "dark" ? '&#9790;' : '&#9728;';
+			toggleButton.style.backgroundColor = newTheme === "dark" ? '#333' : '#f5f5f5';
+			toggleButton.style.color = newTheme === "dark" ? '#f5f5f5' : '#333';
+		}
+}
