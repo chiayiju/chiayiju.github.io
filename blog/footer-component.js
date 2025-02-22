aba65dc6570beadf56653cf6937f80eb2dd30bc9
Chia-Yi Ju <57925324+chiayiju@users.noreply.github.com>
2025-02-22 11:23:08 +0800

    Add files via upload

--- blog/footer-component.js
+++ blog/footer-component.js
@@ -1,12 +1,10 @@
 class FooterComponent extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `
-      <footer>
-        <p style = "position: relative; top: 1rem; text-align: center">
-            Department of Physics, National Sun Yat-sen University, No. 70 Lien-hai Road, Kaohsiung, Taiwan 804201
-        </p>
-      </footer>
-    `;
-  }
+	connectedCallback() {
+	this.innerHTML = `
+		<footer style = "position: fixed; bottom: 0; width: 100%; text-align: center; background: #000; font-size: 20px;">
+			<a href = "https://phys.nsysu.edu.tw" target = "_blank">Department of Physics</a>, <a href = "https://www.nsysu.edu.tw" target = "_blank">National Sun Yat-sen University</a>, No. 70 Lien-hai Road, Kaohsiung, Taiwan 804201
+		</footer>
+	`;
+	}
 }
 customElements.define('footer-component', FooterComponent);
