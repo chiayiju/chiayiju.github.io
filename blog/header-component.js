01aa12ccf16b8e82f971cd58e08bcbd49984c829
Chia-Yi Ju <57925324+chiayiju@users.noreply.github.com>
2025-04-02 18:08:10 +0800

    Add files via upload

--- blog/header-component.js
+++ blog/header-component.js
@@ -4,7 +4,7 @@ class HeaderComponent extends HTMLElement {
         	<style>
 			    body {
 				    display: none;
-				    padding-bottom: 4rem;
+				    padding-bottom: 2rem;
 			    }
 
 			    .responsive-text-tiny {
@@ -24,7 +24,7 @@ class HeaderComponent extends HTMLElement {
 			    }
 			    
 			    .enlarge {
-					display: inline-block;
+				    display: inline-block;
 					transition: transform 0.3s ease;
 				}
 
@@ -35,16 +35,17 @@ class HeaderComponent extends HTMLElement {
 			
             <header>
                 <h1><a href = "../index.html" style = "margin-left: 5rem; position: relative; top:0px;"><img id = "logo" src = "../images/logo-light.webp" alt = "Formal Theory" height = "100" class = "enlarge"></a></h1>
-                <div style = "position: relative; top: -70px;">
+                <div style = "position: relative; margin-top: -4rem;">
                     <h2>
                         <nav style = "text-align: right; margin-right: 10rem;">
-                            <a href = "../index.html" style = "margin-left: 4rem;" class = "enlarge">Home</a>
-                            <a href = "../members.html" style = "margin-left: 4rem;" class = "enlarge">Members</a>
-                            <a href = "../publications.html" style = "margin-left: 4rem;" class = "enlarge">Publications</a>
-                            <a href = "../blog.html" style = "margin-left: 4rem;" class = "enlarge">Blog</a>
-                            <a href = "../students.html" style = "margin-left: 4rem;" class = "enlarge">For Students</a>
-                            <a href = "../links.html" style = "margin-left: 4rem;" class = "enlarge">Links</a>
-                            <button id = "theme-toggle" style = "width: 40px; height: 40px; border-radius: 50%; border: none; font-size: 20px; cursor: pointer; position: absolute; top: -10px; right: 40px; text-align: center; background-color: #f5f5f5; color: #333;" onclick = "toggleTheme()" class = "enlarge">&#9728;</button>
+                            <a href = "../index.html" style = "margin-left: 4rem; font-size: clamp(12.5px, 2.5vw, 25px);" class = "enlarge">Home</a>
+                            <a href = "../members.html" style = "margin-left: 4rem; font-size: clamp(12.5px, 2.5vw, 25px);" class = "enlarge">Members</a>
+                            <a href = "../publications.html" style = "margin-left: 4rem; font-size: clamp(12.5px, 2.5vw, 25px);" class = "enlarge">Publications</a>
+                            <a href = "../blog.html" style = "margin-left: 4rem; font-size: clamp(12.5px, 2.5vw, 25px);" class = "enlarge">Blog</a>
+                            <a href = "../students.html" style = "margin-left: 4rem; font-size: clamp(12.5px, 2.5vw, 25px);" class = "enlarge">For Students</a>
+                            <a href = "../links.html" style = "margin-left: 4rem; font-size: clamp(12.5px, 2.5vw, 25px);" class = "enlarge">Links</a>
+                            
+                            <button id = "theme-toggle" style = "width: 2rem; height: 2rem; border-radius: 50%; border: none; font-size: clamp(10px, 2vw, 20px); cursor: pointer; position: absolute; margin-top: -0.4rem; right: 3rem; text-align: center; background-color: #f5f5f5; color: #333;" onclick = "toggleTheme()" class = "enlarge">&#9728;</button>
                         </nav>
                     </h2>
                 </div>
