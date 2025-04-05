8a460b0e2a690846387dde83d248bde59abc9f03
Chia-Yi Ju <57925324+chiayiju@users.noreply.github.com>
2025-04-05 09:09:38 +0800

    Add files via upload

--- blog/header-component.js
+++ blog/header-component.js
@@ -73,7 +73,7 @@ class HeaderComponent extends HTMLElement {
 					}
 
 				/* Mobile styles */
-				@media (max-width: 1280px) {
+				@media (max-width: 1370px) {
 					body {
 						padding-bottom: 0rem;
 					}
