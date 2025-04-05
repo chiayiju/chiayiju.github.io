ac1d1340859a0138366b0dd70016368a78180acb
Chia-Yi Ju <57925324+chiayiju@users.noreply.github.com>
2025-04-05 11:28:49 +0800

    Add files via upload

--- blog/header-component.js
+++ blog/header-component.js
@@ -105,7 +105,7 @@ class HeaderComponent extends HTMLElement {
 						padding: 1rem 2rem;
 						border-radius: 0.8rem;
 						position: absolute;
-						top: 4rem;
+						top: 4.5rem;
 						right: 1rem;
 						z-index: 1000;
 						gap: 0rem;
